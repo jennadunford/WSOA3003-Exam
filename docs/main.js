66b7a3ab34b3a2bf1545118850ff1e8775e65ec1
pancakes2000 <47218521+jennadunford@users.noreply.github.com>
2022-06-25 21:15:45 +0200

    second prototype

---
+++ docs/main.js
@@ -0,0 +1,58 @@
+var brainAid = 0;
+var attacker = 0;
+
+//brainAid number identifiers
+//Methylphenidate = 1
+//Clobazam = 2
+//SSRI = 3
+//BetaBlock = 4
+//Self-Care = 5
+
+//Attacker number identifiers
+//Anxiety = 1
+//Depression = 2
+//Trauma = 3
+//Burnout = 4
+
+var mEnergyAttack = 20;
+var mDamageDepr = 25;
+var mFocusAttack = 30;
+
+var cRestAttack = 30;
+var cDamageDepr = 20;
+var cDamageAnx = 20;
+
+var sBalanceCh = 20;
+var sDamageDepr = 30;
+var sDamageAnx = 30;
+
+var bStressAttack = 15;
+var bDamageAnx = 20;
+var bStressAttack = 20;
+
+var scStressAttack = 10;
+var scEnergyAttack = 15;
+
+var anxHeartStress = 20;
+var anxFocusStress = 20;
+var anxEmotionAttack = 30;
+
+var depFatigueAttack = 30;
+var depOverwhlEmot = 25;
+var depEnergyAttack = 30;
+
+var trHealthAttack = 20;
+var trEnergyAttack = 20;
+var trMedEffect = 30;
+
+var brnFocusAttack = 20;
+var brnNoRest = 20;
+var brnBrainTired = 30;
+
+var multiplier = 3;
+
+var applyMultiplier = false;
+
+var divider = 2;
+
+var applyDivider = false;
